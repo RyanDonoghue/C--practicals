2ea696b77802eae33e344512a19e8112e389d667
RyanDonoghue <ryandonoghue26@gmail.com>
2016-09-26 10:40:16 +0100

    Add files via upload

---
+++ P1_MultiConvertor/P1_MultiConvertor/MultiConv.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace P1_MultiConvertor
+{
+    partial class MultiConv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_f2i = new System.Windows.Forms.Button();
+            this.btn_i2f = new System.Windows.Forms.Button();
+            this.btn_e2p = new System.Windows.Forms.Button();
+            this.btn_c2f = new System.Windows.Forms.Button();
+            this.btn_p2e = new System.Windows.Forms.Button();
+            this.btn_f2c = new System.Windows.Forms.Button();
+            this.f2ibox_1 = new System.Windows.Forms.TextBox();
+            this.p2ebox_2 = new System.Windows.Forms.TextBox();
+            this.c2fbox_2 = new System.Windows.Forms.TextBox();
+            this.f2ibox_2 = new System.Windows.Forms.TextBox();
+            this.c2fbox_1 = new System.Windows.Forms.TextBox();
+            this.p2ebox_1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.f2cbox_1 = new System.Windows.Forms.TextBox();
+            this.e2pbox_2 = new System.Windows.Forms.TextBox();
+            this.e2pbox_1 = new System.Windows.Forms.TextBox();
+            this.i2fbox_2 = new System.Windows.Forms.TextBox();
+            this.i2fbox_1 = new System.Windows.Forms.TextBox();
+            this.f2cbox_2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_f2i
+            // 
+            this.btn_f2i.Location = new System.Drawing.Point(12, 51);
+            this.btn_f2i.Name = "btn_f2i";
+            this.btn_f2i.Size = new System.Drawing.Size(94, 20);
+            this.btn_f2i.TabIndex = 0;
+            this.btn_f2i.Text = "Convert";
+            this.btn_f2i.UseVisualStyleBackColor = true;
+            this.btn_f2i.Click += new System.EventHandler(this.btn_f2i_Click);
+            // 
+            // btn_i2f
+            // 
+            this.btn_i2f.Location = new System.Drawing.Point(12, 160);
+            this.btn_i2f.Name = "btn_i2f";
+            this.btn_i2f.Size = new System.Drawing.Size(94, 20);
+            this.btn_i2f.TabIndex = 1;
+            this.btn_i2f.Text = "Convert";
+            this.btn_i2f.UseVisualStyleBackColor = true;
+            this.btn_i2f.Click += new System.EventHandler(this.btn_i2f_Click);
+            // 
+            // btn_e2p
+            // 
+            this.btn_e2p.Location = new System.Drawing.Point(112, 160);
+            this.btn_e2p.Name = "btn_e2p";
+            this.btn_e2p.Size = new System.Drawing.Size(94, 20);
+            this.btn_e2p.TabIndex = 2;
+            this.btn_e2p.Text = "Convert";
+            this.btn_e2p.UseVisualStyleBackColor = true;
+            this.btn_e2p.Click += new System.EventHandler(this.btn_e2p_Click);
+            // 
+            // btn_c2f
+            // 
+            this.btn_c2f.Location = new System.Drawing.Point(212, 51);
+            this.btn_c2f.Name = "btn_c2f";
+            this.btn_c2f.Size = new System.Drawing.Size(94, 20);
+            this.btn_c2f.TabIndex = 3;
+            this.btn_c2f.Text = "Convert";
+            this.btn_c2f.UseVisualStyleBackColor = true;
+            this.btn_c2f.Click += new System.EventHandler(this.btn_c2f_Click);
+            // 
+            // btn_p2e
+            // 
+            this.btn_p2e.Location = new System.Drawing.Point(112, 51);
+            this.btn_p2e.Name = "btn_p2e";
+            this.btn_p2e.Size = new System.Drawing.Size(94, 20);
+            this.btn_p2e.TabIndex = 4;
+            this.btn_p2e.Text = "Convert";
+            this.btn_p2e.UseVisualStyleBackColor = true;
+            this.btn_p2e.Click += new System.EventHandler(this.btn_p2e_Click);
+            // 
+            // btn_f2c
+            // 
+            this.btn_f2c.Location = new System.Drawing.Point(212, 160);
+            this.btn_f2c.Name = "btn_f2c";
+            this.btn_f2c.Size = new System.Drawing.Size(94, 20);
+            this.btn_f2c.TabIndex = 5;
+            this.btn_f2c.Text = "Convert";
+            this.btn_f2c.UseVisualStyleBackColor = true;
+            this.btn_f2c.Click += new System.EventHandler(this.btn_f2c_Click);
+            // 
+            // f2ibox_1
+            // 
+            this.f2ibox_1.Location = new System.Drawing.Point(12, 25);
+            this.f2ibox_1.Name = "f2ibox_1";
+            this.f2ibox_1.Size = new System.Drawing.Size(94, 20);
+            this.f2ibox_1.TabIndex = 6;
+            // 
+            // p2ebox_2
+            // 
+            this.p2ebox_2.Location = new System.Drawing.Point(112, 80);
+            this.p2ebox_2.Name = "p2ebox_2";
+            this.p2ebox_2.Size = new System.Drawing.Size(94, 20);
+            this.p2ebox_2.TabIndex = 7;
+            // 
+            // c2fbox_2
+            // 
+            this.c2fbox_2.Location = new System.Drawing.Point(212, 80);
+            this.c2fbox_2.Name = "c2fbox_2";
+            this.c2fbox_2.Size = new System.Drawing.Size(94, 20);
+            this.c2fbox_2.TabIndex = 8;
+            // 
+            // f2ibox_2
+            // 
+            this.f2ibox_2.Location = new System.Drawing.Point(12, 80);
+            this.f2ibox_2.Name = "f2ibox_2";
+            this.f2ibox_2.Size = new System.Drawing.Size(94, 20);
+            this.f2ibox_2.TabIndex = 9;
+            // 
+            // c2fbox_1
+            // 
+            this.c2fbox_1.Location = new System.Drawing.Point(212, 25);
+            this.c2fbox_1.Name = "c2fbox_1";
+            this.c2fbox_1.Size = new System.Drawing.Size(94, 20);
+            this.c2fbox_1.TabIndex = 10;
+            // 
+            // p2ebox_1
+            // 
+            this.p2ebox_1.Location = new System.Drawing.Point(112, 25);
+            this.p2ebox_1.Name = "p2ebox_1";
+            this.p2ebox_1.Size = new System.Drawing.Size(94, 20);
+            this.p2ebox_1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Feet to Inches";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "GBP to EUR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Celsius to Farenheit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Inches to Feet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Farenheit to Celsius";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(109, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "EUR to GBP";
+            // 
+            // f2cbox_1
+            // 
+            this.f2cbox_1.Location = new System.Drawing.Point(212, 134);
+            this.f2cbox_1.Name = "f2cbox_1";
+            this.f2cbox_1.Size = new System.Drawing.Size(94, 20);
+            this.f2cbox_1.TabIndex = 18;
+            // 
+            // e2pbox_2
+            // 
+            this.e2pbox_2.Location = new System.Drawing.Point(112, 186);
+            this.e2pbox_2.Name = "e2pbox_2";
+            this.e2pbox_2.Size = new System.Drawing.Size(94, 20);
+            this.e2pbox_2.TabIndex = 19;
+            // 
+            // e2pbox_1
+            // 
+            this.e2pbox_1.Location = new System.Drawing.Point(112, 134);
+            this.e2pbox_1.Name = "e2pbox_1";
+            this.e2pbox_1.Size = new System.Drawing.Size(94, 20);
+            this.e2pbox_1.TabIndex = 20;
+            // 
+            // i2fbox_2
+            // 
+            this.i2fbox_2.Location = new System.Drawing.Point(12, 186);
+            this.i2fbox_2.Name = "i2fbox_2";
+            this.i2fbox_2.Size = new System.Drawing.Size(94, 20);
+            this.i2fbox_2.TabIndex = 21;
+            // 
+            // i2fbox_1
+            // 
+            this.i2fbox_1.Location = new System.Drawing.Point(12, 134);
+            this.i2fbox_1.Name = "i2fbox_1";
+            this.i2fbox_1.Size = new System.Drawing.Size(94, 20);
+            this.i2fbox_1.TabIndex = 22;
+            // 
+            // f2cbox_2
+            // 
+            this.f2cbox_2.Location = new System.Drawing.Point(212, 186);
+            this.f2cbox_2.Name = "f2cbox_2";
+            this.f2cbox_2.Size = new System.Drawing.Size(94, 20);
+            this.f2cbox_2.TabIndex = 23;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Clear all";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 243);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.f2cbox_2);
+            this.Controls.Add(this.i2fbox_1);
+            this.Controls.Add(this.i2fbox_2);
+            this.Controls.Add(this.e2pbox_1);
+            this.Controls.Add(this.e2pbox_2);
+            this.Controls.Add(this.f2cbox_1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p2ebox_1);
+            this.Controls.Add(this.c2fbox_1);
+            this.Controls.Add(this.f2ibox_2);
+            this.Controls.Add(this.c2fbox_2);
+            this.Controls.Add(this.p2ebox_2);
+            this.Controls.Add(this.f2ibox_1);
+            this.Controls.Add(this.btn_f2c);
+            this.Controls.Add(this.btn_p2e);
+            this.Controls.Add(this.btn_c2f);
+            this.Controls.Add(this.btn_e2p);
+            this.Controls.Add(this.btn_i2f);
+            this.Controls.Add(this.btn_f2i);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_f2i;
+        private System.Windows.Forms.Button btn_i2f;
+        private System.Windows.Forms.Button btn_e2p;
+        private System.Windows.Forms.Button btn_c2f;
+        private System.Windows.Forms.Button btn_p2e;
+        private System.Windows.Forms.Button btn_f2c;
+        private System.Windows.Forms.TextBox f2ibox_1;
+        private System.Windows.Forms.TextBox p2ebox_2;
+        private System.Windows.Forms.TextBox c2fbox_2;
+        private System.Windows.Forms.TextBox f2ibox_2;
+        private System.Windows.Forms.TextBox c2fbox_1;
+        private System.Windows.Forms.TextBox p2ebox_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox f2cbox_1;
+        private System.Windows.Forms.TextBox e2pbox_2;
+        private System.Windows.Forms.TextBox e2pbox_1;
+        private System.Windows.Forms.TextBox i2fbox_2;
+        private System.Windows.Forms.TextBox i2fbox_1;
+        private System.Windows.Forms.TextBox f2cbox_2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ P1_MultiConvertor/P1_MultiConvertor/MultiConv.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1_MultiConvertor
+{
+    public partial class MultiConv : Form
+    {
+        //field names
+        // f2ibox_1
+        // f2ibox_2
+        // i2fbox_1
+        // i2fbox_2
+        // g2ebox_1
+        // g2ebox_2
+        // e2gbox_1
+        // e2gbox_2
+        // c2f_box1
+        // c2fbox_2
+        // f2cbox_1
+        // f2cbox_2
+
+        // conversion rates
+        decimal f2irate = 12M;
+        decimal i2frate = 0.0833333M;
+        decimal p2erate = 1.17M;
+        decimal e2prate = 0.86M;
+        decimal c2frate = 0.5556M;
+        decimal f2crate = 1.8M;
+
+
+
+
+        public MultiConv()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_f2i_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(f2ibox_1.Text) == false)
+            {
+                f2ibox_2.Text = "";
+                f2ibox_2.Text += decimal.Multiply(decimal.Parse(f2ibox_1.Text), f2irate);
+            }
+        }
+
+        private void btn_p2e_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(p2ebox_1.Text) == false)
+            {
+                p2ebox_2.Text = "";
+                p2ebox_2.Text += decimal.Multiply(decimal.Parse(p2ebox_1.Text), p2erate);
+            }
+        }
+
+        private void btn_c2f_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(c2fbox_1.Text) == false)
+            {
+                c2fbox_2.Text = "";
+                c2fbox_2.Text += decimal.Multiply((decimal.Parse(c2fbox_1.Text) - 32), c2frate );
+            }
+        }
+
+        private void btn_i2f_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(i2fbox_1.Text) == false)
+            {
+                i2fbox_2.Text = "";
+                i2fbox_2.Text += decimal.Multiply(decimal.Parse(i2fbox_1.Text), i2frate);
+            }
+        }
+
+        private void btn_e2p_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(e2pbox_1.Text) == false)
+            {
+                e2pbox_2.Text = "";
+                e2pbox_2.Text += decimal.Multiply(decimal.Parse(e2pbox_1.Text), e2prate);
+            }
+        }
+
+        private void btn_f2c_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(f2cbox_1.Text) == false)
+            {
+                f2cbox_2.Text = "";
+                f2cbox_2.Text += decimal.Multiply(decimal.Parse(f2ibox_1.Text), f2crate) + 32;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f2cbox_2.Text = "";
+            e2pbox_2.Text = "";
+            i2fbox_2.Text = "";
+            c2fbox_2.Text = "";
+            p2ebox_2.Text = "";
+            f2ibox_2.Text = "";
+        }
+    }
+}
